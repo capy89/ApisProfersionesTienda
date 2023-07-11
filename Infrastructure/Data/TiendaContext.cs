9b67063e635d4e97a9873f84730f397f6fc38e21
Carlos Alberto Peña Yañez <capy89@gmail.com>
2023-07-11 17:46:22 -0600

    Added new entities, marcas and categorias

--- Infrastructure/Data/TiendaContext.cs
+++ Infrastructure/Data/TiendaContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,5 +17,16 @@ namespace Infrastructure.Data
         }
 
         public DbSet<Producto> Productos { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+
     }
 }
