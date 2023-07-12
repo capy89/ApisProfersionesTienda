d0d74427624b5ad1a1806a8372938569c1e9f8eb
Carlos Alberto Peña Yañez <capy89@gmail.com>
2023-07-11 19:07:38 -0600

    Added the first controller, ProductosController

---
+++ Api/Controllers/ProductosController.cs
@@ -0,0 +1,44 @@
+﻿
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    public class ProductosController : BaseApiController
+    {
+        public TiendaContext _context { get; }
+        public ProductosController(TiendaContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<Producto>>> Get()
+        {
+            var productos = await _context.Productos
+                .ToListAsync();
+
+            return Ok(productos);
+
+
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult> Get(int id)
+        {
+            var producto = await _context.Productos
+                .FindAsync(id);
+
+            return Ok(producto);
+
+
+        }
+
+    }
+}
