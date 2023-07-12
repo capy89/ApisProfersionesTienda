cbee0d3755d222224db645c8fa62b53f92d35498
Carlos Alberto Peña Yañez <capy89@gmail.com>
2023-07-11 19:31:32 -0600

    Create Repository generic and repository productos

---
+++ Infrastructure/Repositories/ProductosRepository.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public class ProductosRepository : GenericRepository<Producto>, IProductoRepository
+    {
+        public ProductosRepository(TiendaContext context) :base(context)
+        {
+            
+        }
+
+        public async Task<IEnumerable<Producto>> GetProductosMasCaros(int cantidad) =>
+            await _context.Productos
+            .OrderByDescending(p => p.Precio)
+            .Take(cantidad)
+            .ToListAsync();
+    }
+}
