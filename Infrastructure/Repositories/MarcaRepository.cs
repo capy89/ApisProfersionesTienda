fbe44646803a18ba5def090f0a9a9f19abb06f9e
Carlos Alberto Peña Yañez <capy89@gmail.com>
2023-07-11 19:35:44 -0600

    added repositories of marcas and categorias

---
+++ Infrastructure/Repositories/MarcaRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    internal class MarcaRepository : GenericRepository<Marca>, IMarcaRepository
+    {
+        public MarcaRepository(TiendaContext context) : base(context)
+        {
+            
+        }
+
+
+    }
+}
