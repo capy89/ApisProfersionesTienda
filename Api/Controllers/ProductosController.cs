d15d6effbb8782c7d88673362d7be3e2ec0465b0
Carlos Alberto Peña Yañez <capy89@gmail.com>
2023-07-12 18:21:47 -0600

    Added the actions Post,Put and Delete in ProductosController

--- Api/Controllers/ProductosController.cs
+++ Api/Controllers/ProductosController.cs
@@ -41,5 +41,58 @@ namespace Api.Controllers
 
         }
 
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Producto>> Post(Producto producto)
+        {
+            _unitOfWork.Productos.Add(producto);
+            _unitOfWork.Save();
+
+            if (producto == null)
+            {
+                return BadRequest();
+            }
+
+            return CreatedAtAction(nameof(Post),new { id = producto.Id }, producto);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<Producto>> Put(int id, [FromBody]Producto producto)
+        {
+
+            if(producto == null)
+                return NotFound();
+
+            _unitOfWork.Productos.Update(producto);
+            _unitOfWork.Save();
+
+
+            return producto;
+
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult> Delete(int id)
+        {
+
+            var producto=await _unitOfWork.Productos.GetByIdAsync(id);
+
+            if (producto == null)
+                return NotFound();
+
+            _unitOfWork.Productos.Remove(producto);
+            _unitOfWork.Save();
+
+
+            return NoContent();
+
+        }
+
     }
 }
--- Core/Entities/Producto.cs
+++ Core/Entities/Producto.cs
@@ -8,17 +8,17 @@ namespace Core.Entities
 {
     public class Producto : BaseEntity
     {
-        public string Nombre { get; set; }
+        public string Nombre { get; set; } = string.Empty;
         public decimal Precio { get; set; }
         public DateTime FechaCreacion { get; set; }
 
         public int MarcaId { get; set; }
 
-        public Marca Marca { get; set; }
+        public Marca? Marca { get; set; }
 
         public int CategoriaId { get; set; }
 
-        public Categoria Categoria { get; set; }
+        public Categoria? Categoria { get; set; }
 
     }
 }
